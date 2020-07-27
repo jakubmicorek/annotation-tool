a2a630a7de9dd697a8e1d49e43b19e8dcdb0a7b6
Jakub <jakub.micorek@icg.tugraz.at>
2020-07-27 09:44:20 +0200

    labeles

--- show_labels.py
+++ show_labels.py
@@ -23,13 +23,16 @@ print("video id:", video_id)
 print("load video:", f"{args.input_folder}/{args.video_name}")
 cap = cv2.VideoCapture(f'{args.input_folder}/{args.video_name}')
 
-with open(f'{args.input_folder}/{args.ouput_folder_name}/labeled_{video_id}.txt', "r") as f:
+file_name = f'{args.input_folder}/{args.ouput_folder_name}/labeled_{video_id}.txt'
+print(file_name)
+with open(file_name, "r") as f:
     print(f.readline())
     label_lines = [line.strip().split(",") for line in f]
     labels = {int(line[4]): line[3] for line in label_lines}
 
 print("labels:", labels)
 frame_number = 0
+old_label = None
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -39,11 +42,22 @@ while cap.isOpened():
         break
 
     label = labels[frame_number] if frame_number in labels else "no label"
-    frame = cv2.putText(frame, label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+    color = (0, 0, 255) if frame_number in labels else (255, 0, 255)
+    frame = cv2.putText(frame, label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
     cv2.imshow('frame', frame)
 
-    cv2.waitKey(70)
+    if frame_number not in labels:
+        print("unlabeled frame:", frame_number)
+        cv2.waitKey(0)
+
+    # if old_label != label:
+    #     cv2.waitKey(0)
+
+    # cv2.waitKey(70)
+    cv2.waitKey(2)
+    # cv2.waitKey(1)
     frame_number += 1
+    old_label = label
 
 cap.release()
 cv2.destroyAllWindows()
